5d476ec313e6b86c382ab03601cfcca597090ccb
jalbertort <jramireztolentino@gmail.com>
2024-03-07 18:52:17 -0400

    Add enable CORS

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,17 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  
+  app.enableCors();
+  
   app.useGlobalPipes(
     new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
     })
-   );
-
-  await app.listen(3000);
+    );
+    
+    await app.listen(3000);
+    
 }
 bootstrap();
